bf9c939d8ff4f783cc40ec18d37efbb3090780b3
mHamilton91 <m.hamilton2891@gmail.com>
2019-08-18 16:39:17 -0500

    Uploading Inital Files

---
+++ script.js
@@ -0,0 +1,10 @@
+function rotateFunction() {
+		document.getElementById("barContainer").classList.toggle("rotateChange");
+		document.getElementById("dropdown").classList.toggle("dropColorChange");
+		document.getElementById("name").classList.toggle("fadeName");
+		
+	}	
+
+$(document).ready(function() {
+	
+});
