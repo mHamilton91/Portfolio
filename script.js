726e5155034bb7e084056e4c7abc220489c7b830
mHamilton91 <m.hamilton2891@gmail.com>
2019-08-25 01:42:34 -0500

    Added images to the graphics page and slideshow functionality

--- script.js
+++ script.js
@@ -5,6 +5,24 @@ function rotateFunction() {
 		
 	}	
 
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+	showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+	var i;
+	var x = document.getElementsByClassName("mySlides");
+	if (n > x.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = x.length}
+	for (i = 0; i < x.length; i++) {
+	x[i].style.display = "none";  
+	}
+	x[slideIndex-1].style.display = "block";  
+}
+
 	/*document.getElementsById("name").onclick = function(event) {
 		event.preventDefault();
 
