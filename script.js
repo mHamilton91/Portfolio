ba8772868d7bb48df3d8e8b1f6d64f15d8d11f23
mHamilton91 <m.hamilton2891@gmail.com>
2019-08-29 03:25:48 -0500

    Removed if statement in script

--- script.js
+++ script.js
@@ -5,38 +5,36 @@ function rotateFunction() {
 		
 	}	
 
-if(window.location.pathname == '/graphics.html'){
-	var slideIndex = 1;
-	slideShow(slideIndex);
+var slideIndex = 1;
+slideShow(slideIndex);
 
-	function slideChange(n) {
-		slideShow(slideIndex += n);
-	}
+function slideChange(n) {
+	slideShow(slideIndex += n);
+}
 
-	function slideShow(n) {
-		var images = ["GhostBeer.png",
-						"GreyjoyBeer.png",
-						"lannisterBeer.png",
-						"nightsWatchBeer.png",
-						"targaryenBeer.png",
-						"winterfellBeer.png",
-						"leonaGlitch.png",
-						"leonaParticle.png",
-						"leonaAlbum.png",
-						"Thieves_Hollow_Logo_Name.png",
-						"Thieves_Hollow_Logo_Name_Pizza.png",
-						"Thieves_Hollow_Logo_Name_Purple.png",
-						"Thieves_Hollow_Logo_Name_Space.png",
-						"Thieves_Hollow_Logo_Name_Stripes.png",
-						"backspaceFlyer.png",
-						"ForestTriangle.jpg",
-						"greenCave.png",
-						"cave.png"]
-		if (n > 18) {slideIndex = 1}
-		if (n < 1) {slideIndex = 18}
-		let imgSlide = document.querySelector(".slider");
+function slideShow(n) {
+	var images = ["GhostBeer.png",
+					"GreyjoyBeer.png",
+					"lannisterBeer.png",
+					"nightsWatchBeer.png",
+					"targaryenBeer.png",
+					"winterfellBeer.png",
+					"leonaGlitch.png",
+					"leonaParticle.png",
+					"leonaAlbum.png",
+					"Thieves_Hollow_Logo_Name.png",
+					"Thieves_Hollow_Logo_Name_Pizza.png",
+					"Thieves_Hollow_Logo_Name_Purple.png",
+					"Thieves_Hollow_Logo_Name_Space.png",
+					"Thieves_Hollow_Logo_Name_Stripes.png",
+					"backspaceFlyer.png",
+					"ForestTriangle.jpg",
+					"greenCave.png",
+					"cave.png"]
+	if (n > 18) {slideIndex = 1}
+	if (n < 1) {slideIndex = 18}
+	let imgSlide = document.querySelector(".slider");
 
-		imgSlide.style.background = `url(${images[slideIndex-1]}) no-repeat scroll center center`;
-		imgSlide.style.backgroundSize = "contain";
-	}
+	imgSlide.style.background = `url(${images[slideIndex-1]}) no-repeat scroll center center`;
+	imgSlide.style.backgroundSize = "contain";
 }
